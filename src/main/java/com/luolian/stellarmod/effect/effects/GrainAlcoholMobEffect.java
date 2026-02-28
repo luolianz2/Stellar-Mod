188ba3b9bcbb153f88df12b6fbd840d15259437c
GRAINALCOHOL <132149646+GRAINALCOHOL@users.noreply.github.com>
2026-02-28 22:16:55 +0800

    2026-02-28 fix: 修复了在生产环境中mixin报错的问题 fix: 修复了GrainAlcoholMobEffect错误地引用已删除类的问题

--- src/main/java/com/luolian/stellarmod/effect/effects/GrainAlcoholMobEffect.java
+++ src/main/java/com/luolian/stellarmod/effect/effects/GrainAlcoholMobEffect.java
@@ -1,7 +1,5 @@
 package com.luolian.stellarmod.effect.effects;
 
-import com.luolian.stellarmod.listener.MobEffectEventListener;
-import com.luolian.stellarmod.potion.StellarPotions;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.item.ItemStack;
@@ -9,10 +7,9 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraftforge.event.entity.living.MobEffectEvent;
 
 /**
- * 状态效果功能详见{@linkplain MobEffectEventListener#onMobEffectAdded(MobEffectEvent.Added)}
+ * 状态效果功能详见{@linkplain com.luolian.stellarmod.mixin.LivingEntityMixin}
  */
 public class GrainAlcoholMobEffect extends MobEffect {
     public GrainAlcoholMobEffect() {
